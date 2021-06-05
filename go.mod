f950a32fca2eaa063c76b2f01294a31c0fe5bfba
Ignacio Hagopian <jsign.uy@gmail.com>
2021-06-05 17:35:54 -0300

    first impl
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/jsign/ds-bench
 
 go 1.16
+
+require (
+	github.com/ipfs/go-datastore v0.4.5
+	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/textileio/go-ds-badger3 v0.0.0-20210324034212-7b7fb3be3d1c
+	github.com/textileio/go-ds-mongo v0.1.4
+)
--- main.go
+++ main.go
@@ -3,21 +3,110 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"sync"
+	"sync/atomic"
 	"time"
-   badger "github.com/textileio/go-ds-badger3"                                                                                                                                     mongods "github.com/textileio/go-ds-mongo"                                                                                                                            
+
+	"github.com/ipfs/go-datastore"
+	"github.com/kataras/tablewriter"
+	badger "github.com/textileio/go-ds-badger3"
+	mongods "github.com/textileio/go-ds-mongo"
 )
 
 func main() {
+	bench()
 }
 
+func bench() {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"size", "count", "parallel", "txn"})
 
-  // NewBadgerTxnDatastore returns a new txndswrap.TxnDatastore backed by Badger.                                                                                                 func NewBadgerTxnDatastore(repoPath string) (txndswrap.TxnDatastore, error) {                                                                                                 
-          if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {                                                                                                                              return nil, err                                                                                                                                               
-          }                                                                                                                                                                               return badger.NewDatastore(repoPath, &badger.DefaultOptions)                                                                                                          
-  }
+	sizesExp := []int{1, 10, 20}
+	counts := []int{1, 2, 4, 10}
+	withTxns := []bool{false, true}
+	parallels := []int{1}
+	//sizesExp := []int{1, 10, 20}
+	//	counts := []int{1, 2, 4, 10, 50, 100}
+	//	withTxns := []bool{false, true}
+	//	parallels := []int{1, 2, 5, 10, 50, 100}
+
+	for _, sizeExp := range sizesExp {
+		for _, count := range counts {
+			for _, parallel := range parallels {
+				for _, withTxn := range withTxns {
+					durationMs, numErrors := benchCase(sizeExp, count, parallel, withTxn)
+					r := []string{
+						strconv.Itoa(1 << sizeExp),
+						strconv.Itoa(count),
+						strconv.Itoa(parallel),
+						strconv.FormatBool(withTxn),
+						strconv.FormatInt(durationMs, 10),
+						strconv.FormatInt(numErrors, 10),
+					}
+					table.Append(r)
+				}
+			}
+		}
+	}
+
+	table.Render()
+}
+
+func benchCase(sizeExp, count, parallel int, withTxn bool) (int64, int64) {
+	tmpDir, err := ioutil.TempDir("", "")
+	checkErr(err)
+	defer os.RemoveAll(tmpDir)
+	ds, err := NewBadgerTxnDatastore(tmpDir)
+	checkErr(err)
+
+	data := make([]byte, 1<<sizeExp)
+
+	var wg sync.WaitGroup
+	wg.Add(parallel)
+
+	var totalDuration int64
+	var numErrors int64
+	for k := 0; k < parallel; k++ {
+		k := k
+		go func() {
+			defer wg.Done()
+			w := ds.(datastore.Write)
+			if withTxn {
+				txn, err := ds.NewTransaction(false)
+				if err != nil {
+					panic(err)
+				}
+				w = txn
+			}
+			start := time.Now()
+			for i := 0; i < count; i++ {
+				key := datastore.NewKey(fmt.Sprintf("%d_%d", k, i))
+				if err := w.Put(key, data); err != nil {
+					atomic.AddInt64(&numErrors, 1)
+				}
+			}
+			atomic.AddInt64(&totalDuration, time.Since(start).Milliseconds())
+		}()
+	}
+
+	wg.Wait()
+
+	return atomic.LoadInt64(&totalDuration) / int64(parallel), numErrors
+}
+
+// NewBadgerTxnDatastore returns a new txndswrap.TxnDatastore backed by Badger.
+func NewBadgerTxnDatastore(repoPath string) (datastore.TxnDatastore, error) {
+	if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {
+		return nil, err
+	}
+	return badger.NewDatastore(repoPath, &badger.DefaultOptions)
+}
 
 // NewMongoTxnDatastore returns a new txndswrap.TxnDatastore backed by MongoDB.
-func NewMongoTxnDatastore(uri, dbName string) (txndswrap.TxnDatastore, error) {
+func NewMongoTxnDatastore(uri, dbName string) (datastore.TxnDatastore, error) {
 	mongoCtx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
@@ -33,4 +122,10 @@ func NewMongoTxnDatastore(uri, dbName string) (txndswrap.TxnDatastore, error) {
 	}
 
 	return ds, nil
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
